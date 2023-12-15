c15dd223a1fbed801f1aa0de81733949b81449ee
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2023-12-15 12:24:55 +0300

    Update 2.3.4_paragraph.R

--- 2.3.4_paragraph.R
+++ 2.3.4_paragraph.R
@@ -2,6 +2,7 @@
 #####  R. version: 4.3.2 2023/10/31  #####
 #####        paragraph 2.3.4.1       #####
 #####  from the book "Spatial Ecology and Conservation Modeling" - Springer(2018)  #####
+#####  revisited #####
 
 
 #####    code tested on 12/12/2023   #####
@@ -240,6 +241,7 @@ forest200 <- aggregate(forest, fact=7, fun=modal)
 #####                                #####
 #####        paragraph 2.3.4.2       #####
 #####  from the book "Spatial Ecology and Conservation Modeling" - Springer(2018)  #####
+#####  revisited #####
 
 #####     code tested on 12/14/2023  #####
 
